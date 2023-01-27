73562e8bc18df853e38527ae2dc6c08d974a4131
Yassinenali01 <118232464+Yassinenali01@users.noreply.github.com>
2023-01-27 14:39:59 -0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab1.oop
+{
+    internal class Person
+    {
+        //fields
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favoriteColour;
+        private int age;
+        private bool isWorking;
+
+        public int PersonId { get => personId; set => personId = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FavoriteColour { get => favoriteColour; set => favoriteColour = value; }
+        public int Age { get => age; 
+            set => age = value; }
+        public bool IsWorking { get => isWorking; set => isWorking = value; }
+
+
+        //constructor like the def _init_ in python.
+
+        public Person(int personId, string firstName, string lastName, string favoriteColour, int age, bool isWorking)
+        {
+            this.PersonId = personId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.FavoriteColour = favoriteColour;
+            this.Age = age;
+            this.IsWorking = isWorking;
+
+        }
+        //methods
+        public void DisplayPersonInfo()
+        {
+
+            Console.WriteLine(FirstName + LastName);
+            Console.WriteLine(this.FirstName+"'s favorite colour is "+this.FavoriteColour);
+
+        }
+        public void ChangeFavoriteColour()
+        {
+            this.FavoriteColour = "White";
+            //Console.WriteLine("your new favorite colour is"+this.favoriteColour);
+
+        }
+        public void ChangeAgeInTenYears() 
+        {
+            int newAge = this.Age + 10;
+            Console.WriteLine( FirstName + " your age after ten years is " + newAge);
+        }
+        public override string ToString()
+        {
+            return $" person Id = {PersonId}\n first name = {FirstName}\n last name = {LastName}\n age is = {Age}\n favorite colour is = {FavoriteColour} \n is working = {IsWorking}";
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{PersonId} : {FirstName} {LastName}'s favorite colour is: {FavoriteColour}");
+        }
+
+       /* public void DisplayInfoAsList()
+        {
+            List <object> myList = new List<object>();
+            myList.Add(this.personId);
+            myList.Add(this.firstName);
+            myList.Add(this.lastName);
+            myList.Add(this.favoriteColour);
+            myList.Add(this.age);
+            myList.Add(this.isWorking);
+            *//*Console.WriteLine(myList);*//* 
+            foreach (object item in myList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            
+        }*/
+
+        /*List<Person> people = new List<Person>();*/
+        // for min and max you do for i =0 and i < 4 and i++
+        // you do assign the first person age as a max age and you compare it to other ages.
+        // if a bigger age is found it will be assigned to the max age
+        // same thing for the min age
+
+    }
+
+
+
+
+
+
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Lab1.oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1;
+
+            person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+
+            Person person2;
+
+            person2 = new Person(2, "Gina", "James", "Green", 18, false);
+
+            Person person3;
+
+            person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+
+            Person person4;
+
+            person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+
+            person2.DisplayInfo();
+
+            Console.WriteLine(person3.ToString());
+
+            person1.ChangeFavoriteColour();
+
+            person1.DisplayInfo();
+
+            person4.ChangeAgeInTenYears();
+
+            
+
+
+            List<Person> people = new List<Person>();
+            people.Add(person1);
+            people.Add(person2);
+            people.Add(person3);
+            people.Add(person4);
+
+            int totalAge = 0;
+
+            foreach (Person person in people)
+               
+            {
+                totalAge += person.Age;
+            }
+
+            double averageAge = (double)totalAge / people.Count;
+            Console.WriteLine("the average age: " + averageAge);
+
+
+            // YOUNGEST PERSON 
+            string youngest = "";
+            int minAge = person1.Age;
+            for(int i = 0;i < people.Count;i++)
+            {
+                if (people[i].Age < minAge)
+                {
+                    //minAge = people[i].Age;
+                    youngest = people[i].FirstName; 
+                }
+
+            }
+            Console.WriteLine("the youngest person is "+youngest );
+
+            // OLDEST PERSON
+
+            string oldest = "";
+            int maxAge = person1.Age;
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (people[i].Age > maxAge)
+                {
+                    //maxAge = people[i].age;
+                    oldest = people[i].FirstName;
+                }
+
+            }
+            Console.WriteLine("the oldest person is " + oldest);
+
+            // NAME THAT STARTS WITH AN "M"
+
+            Console.WriteLine("Persons that their name starts with an 'M': ");
+        
+            foreach (Person person in people)
+            {
+                if (person.FirstName.StartsWith("M"))
+                {
+                    Console.WriteLine(person.ToString());
+
+                }
+            }
+            // PERSON WHO LIKES BLUE.
+
+            foreach(Person person in people)
+            {
+                if (person.FavoriteColour =="Blue")
+                { Console.WriteLine(person.ToString()); }
+            }
+            Console.ReadKey();
+        }
+    }
+}
